9ff0b4ebfc5e2dfa95b69d6b66d2b43de867cfe9
iamandaleao <amandabibiano144@gmail.com>
2025-07-14 13:40:09 -0300

    feat: add functions 01

---
+++ function/1.function.js
@@ -0,0 +1,22 @@
+main()
+
+function main(){
+  getData()
+  checkValues()
+  sendToDatabase()
+}
+
+function getData(){
+  if (1 < 3) {
+    console.log("numero encontrado")
+    console.log("numero encontrado")
+    console.log("numero encontrado")
+  }
+}
+function checkValues(){
+  console.log("validando dados")
+}
+
+function sendToDatabase(){
+  console.log("cadastrando dados")
+}

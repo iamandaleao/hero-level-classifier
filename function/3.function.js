6f739a23570701675cb874e6efb4c4784202c4b1
iamandaleao <amandabibiano144@gmail.com>
2025-07-14 14:31:26 -0300

    feat: add more functions

--- function/3.function.js
+++ function/3.function.js
@@ -1,6 +1,6 @@
-torrar("pão de forma", "camisa", 10.9)
+torrar("pão na chapa")
 
-function torrar(pao, nome = "cliente", valor) {
+function torrar(pao, valor =99.90, nome = "cliente") {
   console.log("torrada feita com " + pao)
   console.log("ela é um pedido de " + nome)
   console.log("O valor total é " + valor)

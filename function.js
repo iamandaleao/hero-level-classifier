2dd97065b845472512de8004bc807026b2a5ca77
iamandaleao <amandabibiano144@gmail.com>
2025-07-14 13:16:31 -0300

    feat: update function logic for stock control

---
+++ function.js
@@ -0,0 +1,11 @@
+torrar()
+
+function torrar(){
+  console.log("torrando pão")
+  injetarPao()
+}
+
+function injetarPao(){
+  console.log("preparando para injetar o pão")
+  console.log("finalizado")
+}

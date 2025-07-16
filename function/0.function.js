7f8a5e3348408172189e65d1caca04c754aa5bcd
iamandaleao <amandabibiano144@gmail.com>
2025-07-15 21:11:26 -0300

    chore: format code

--- function/0.function.js
+++ function/0.function.js
@@ -1,11 +1,11 @@
 torrar()
 
-function torrar(){
+function torrar() {
   console.log("torrando pão")
   injetarPao()
 }
 
-function injetarPao(){
+function injetarPao() {
   console.log("preparando para injetar o pão")
   console.log("finalizado")
 }
